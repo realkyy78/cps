d03e8eb9cf95d071ca061548e9ffd6e8ea788fc6
Yun <qazws78@naver.com>
2020-04-20 18:12:17 +0900

    commit

--- indeed.py
+++ indeed.py
@@ -1,10 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
 
-a =[ ]
-for i in range(5) :
-b=[ ]
-for j in range(5) :
-for j in range(3) :
-b.append("0") :
-a.append(b)
+req = requests.get("https://movie.naver.com/movie/running/current.nhn")
+if req.status_code !=200 :
+    print("failed",req.status_code) 
 
-print(a)
+html = req.text
+bs = BeautifulSoup(html, "html.parser")
+
+box = bs.find_all("dl", class_="lst_dsc")
+
+ratio=[]
+title=[]
+
+for b in box :
+    ratio.append(b.find("div",class_="star_t1").find("span",class_="num").text)
+    title.append (b.find("dt", class_="tit").find("a").text)
+
+movieInfo=[]
+for i in range(len(box)) :
+    movie=[]
+    movie.append(title[i])
+    movie.append(ratio[i])
+    movieInfo.append(movie) 
+
+for i in movieInfo:
+    print(i)
